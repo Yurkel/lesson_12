f5ed61d0fca09b5ffb3b7998b9162851aa2b2a76
Yuriy <yuriy-k@ukr.net>
2022-06-13 16:42:02 +0300

    до 16 урока

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {task, series, parallel, src, dest, watch} = require('gulp');
+const { task, series, parallel, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync');
 const postcss = require('gulp-postcss');
@@ -26,34 +26,34 @@ const PLUGINS = [
     ],
     cascade: true
   }),
-  mqpacker({sort: sortCSSmq})
+  mqpacker({ sort: sortCSSmq })
 ];
 
 function scss() {
-  return src(PATH.scssFile).
-    pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).
-    pipe(postcss(PLUGINS)).
-    pipe(dest(PATH.cssFolder)).
-    pipe(browserSync.stream());
+  return src(PATH.scssFile)
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(postcss(PLUGINS))
+    .pipe(dest(PATH.cssFolder))
+    .pipe(browserSync.stream());
 }
 
 function scssDev() {
-  return src(PATH.scssFile, {sourcemaps: true}).
-    pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).
-    pipe(postcss(PLUGINS)).
-    pipe(dest(PATH.cssFolder, {sourcemaps: true})).
-    pipe(browserSync.stream());
+  return src(PATH.scssFile, { sourcemaps: true })
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(postcss([mqpacker({ sort: sortCSSmq })]))
+    .pipe(dest(PATH.cssFolder, { sourcemaps: true }))
+    .pipe(browserSync.stream());
 }
 
 function comb() {
-  return src(PATH.scssFiles).
-    pipe(csscomb()).
-    pipe(dest(PATH.scssFolder));
+  return src(PATH.scssFiles)
+    .pipe(csscomb())
+    .pipe(dest(PATH.scssFolder));
 }
 
 function syncInit() {
   browserSync({
-    server: {baseDir: './'},
+    server: { baseDir: './' },
     notify: false
   });
 }
@@ -70,7 +70,15 @@ function watchFiles() {
   // watch(PATH.cssFiles, sync);
 }
 
+function watchDevFiles() {
+  syncInit();
+  watch(PATH.scssFiles, series(scssDev));
+  watch(PATH.htmlFiles, sync);
+  watch(PATH.jsFiles, sync);
+}
+
 task('comb', series(comb));
 task('scss', series(scss));
 task('dev', series(scssDev));
 task('watch', watchFiles);
+task('watchDev', watchDevFiles);
